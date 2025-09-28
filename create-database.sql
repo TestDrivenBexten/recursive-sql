a2861c12c273b69ee4850009af045bf6e8acb940
Skyler Bexten <skyler.bexten@gmail.com>
2025-09-28 12:18:03 -0700

    Create table Person

--- create-database.sql
+++ create-database.sql
@@ -16,4 +16,20 @@ GO
 
 -- Add any initial table structures or data here if needed
 PRINT 'Switched to Genealogy database.';
+
+-- Create Person table with self-referencing foreign key
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Person]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE Person (
+        PersonID INT IDENTITY(1,1) PRIMARY KEY,
+        FirstName NVARCHAR(50) NOT NULL,
+        ParentID INT NULL,
+        CONSTRAINT FK_Person_Parent FOREIGN KEY (ParentID) REFERENCES Person(PersonID)
+    );
+    PRINT 'Person table created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'Person table already exists.';
+END
 GO
